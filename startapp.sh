cf0ee4cc3d0da48df88edf538eeef34dfd1658da
Mike Hudak <mike@hudaknet.com>
2023-05-06 17:07:12 -0400

    Fixed CIMC_HOST variable

--- startapp.sh
+++ startapp.sh
@@ -4,7 +4,7 @@ echo "Starting app"
 
 cd /app
 
-wget --no-check-certificate -O kvm.jnlp "https://${CIMC_IP}:${CIMC_PORT}/kvm.jnlp?cimcAddr=${CIMC_IP}&tkn1=${CIMC_USER}&tkn2=${CIMC_PASSWORD}"
+wget --no-check-certificate -O kvm.jnlp "https://${CIMC_HOST}:${CIMC_PORT}/kvm.jnlp?cimcAddr=${CIMC_HOST}&tkn1=${CIMC_USER}&tkn2=${CIMC_PASSWORD}"
 
 exec javaws kvm.jnlp &
 
